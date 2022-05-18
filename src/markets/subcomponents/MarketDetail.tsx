ebab2f02ad0c0b9a75c8913e69e4d7aa8edb2f0e
0xAngel <0xangel@waterfalldefi.org>
2022-05-19 01:31:17 +0800

    market detail css

--- src/markets/subcomponents/MarketDetail.tsx
+++ src/markets/subcomponents/MarketDetail.tsx
@@ -3,7 +3,19 @@ type Props = {};
 const MarketDetail: React.FC<Props> = ({}) => {
   return (
     <div className="market-detail-wrapper">
-      <div className="information"></div>
+      <div className="information">
+        <div className="block-wrapper">
+          <div className="block">
+            <span className="portfolio-name">port name</span>
+          </div>
+          <div className="block">
+            <span className="assets"></span>
+          </div>
+          <div className="block">
+            <span className="tvl"></span>
+          </div>
+        </div>
+      </div>
       <div className="charts"></div>
       <div className="content-cd"></div>
     </div>
