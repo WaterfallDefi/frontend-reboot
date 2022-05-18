5d018139aa9bcf8659dafe1fbdcf155a28435705
0xAngel <0xangel@waterfalldefi.org>
2022-05-18 18:55:20 +0800

    markets header row

--- src/markets/Markets.tsx
+++ src/markets/Markets.tsx
@@ -1,7 +1,38 @@
+import "./Markets.scss";
+
 type Props = {};
 
 const Markets: React.FC<Props> = ({}) => {
-  return <div></div>;
+  return (
+    <div className="markets-wrapper">
+      <div className="header-row">
+        <div className="header first">
+          <span>Portfolio Name</span>
+        </div>
+        <div className="header">
+          <span>Asset</span>
+        </div>
+        <div className="header">
+          <span>Cycle</span>
+        </div>
+        <div className="header">
+          <span>Tranche</span>
+        </div>
+        <div className="header">
+          <span>APR</span>
+        </div>
+        <div className="header">
+          <span>Principal</span>
+        </div>
+        <div className="header">
+          <span>Status</span>
+        </div>
+        <div className="header last">
+          <span>Yield</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Markets;
