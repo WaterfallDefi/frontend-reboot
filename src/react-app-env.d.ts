64b18525067a4accd24e16aa28c6bf95b7686c00
0xAngel <0xangel@waterfalldefi.org>
2022-05-25 14:52:46 +0800

    getContract

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+interface Window {
+  ethereum?: {
+    isMetaMask?: true;
+    request?: (...args: any[]) => Promise<void>;
+    on?: (...args: any[]) => void;
+    removeListener?: (...args: any[]) => void;
+    autoRefreshOnNetworkChange?: boolean;
+    chainId: string;
+  };
+}
