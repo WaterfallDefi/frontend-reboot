07ca9520ebb4e90bf0cbe8af92a32fac9be1037b
0xAngel <0xangel@waterfalldefi.org>
2023-09-05 15:24:31 +0800

    new tranchemaster

--- src/config/address.ts
+++ src/config/address.ts
@@ -32,11 +32,12 @@ export const LSD_BenqiStrat: { [network: string]: string } = {
 };
 
 //YEGO Arbitrum Test Contract
-//updated 6/6
+//updated 5th sept
 export const Arbitrum_TrancheMaster: { [network: string]: string } = {
   [NETWORKS.DEVNET]: "",
   [NETWORKS.TESTNET]: "",
-  [NETWORKS.MAINNET]: "0x5787C5DD64861fAbf886A565e55dfd25b7E9D94d",
+  // [NETWORKS.MAINNET]: "0x5787C5DD64861fAbf886A565e55dfd25b7E9D94d",
+  [NETWORKS.MAINNET]: "0xD9B9B5D73E113b05623C3d5d31AeBE35F1e9FFB1",
 };
 
 export const Arbitrum_Ethereum_DepositAddress: { [network: string]: string } = {
